28cdbf9819fc4d9662b703d5cc05aaa8a10bc0ff
Adolfo Benedetti <adolfo.benedetti@gmail.com>
2013-03-15 23:14:06 +0100

    h3-1 done!

--- docs/week2-examples/src/main/java/com/tengen/hwthreeone/FindLowerScore.java
+++ docs/week2-examples/src/main/java/com/tengen/hwthreeone/FindLowerScore.java
@@ -5,6 +5,7 @@ import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
 import com.mongodb.DBCollection;
 import com.mongodb.DBCursor;
+import com.mongodb.DBObject;
 import com.mongodb.MongoClient;
 import com.mongodb.ServerAddress;
 
@@ -29,70 +30,37 @@ public class FindLowerScore {
         DBCursor cursor = studentsCollection.find().sort(new BasicDBObject("student_id", -1));
         try {
             while (cursor.hasNext()) {
-                BasicDBObject currentStudent = (BasicDBObject) cursor.next();
+                DBObject currentStudent = cursor.next();
                 System.out.println(currentStudent);
-                massageCurrentStudent(currentStudent, studentsCollection);
-                // studentsCollection.
+                final BasicDBList scores = (BasicDBList) currentStudent.get("scores");
+                Iterator<Object> iterator = scores.iterator();
+                BasicDBObject firstHWObject = null;
+                while (iterator.hasNext()) {
+                    BasicDBObject secondHWObject = (BasicDBObject) iterator.next();
+                    if (secondHWObject.get("type").toString().equalsIgnoreCase("homework")) {
+                        if (null == firstHWObject) {
+                            System.out.println("==== homework oldscore initializing ====");
+                            firstHWObject = secondHWObject;
+                        } else {
+                            final double firstHWScore = firstHWObject.getDouble("score");
+                            System.out.println(":::: homework oldscore ::::" + firstHWScore);
+                            final double secondHWscore = secondHWObject.getDouble("score");
+                            System.out.println(":::: homework currentscore::::" + secondHWscore);
+                            if (firstHWScore < secondHWscore) {
+                                System.out.println(":::: to remove >>> " + firstHWScore);
+                                studentsCollection.update(new BasicDBObject("_id", currentStudent.get("_id")), new BasicDBObject("$pull", new BasicDBObject("scores", firstHWObject)));
+                            } else {
+                                System.out.println(":::: to remove >>> " + secondHWscore);
+                                studentsCollection.update(new BasicDBObject("_id", currentStudent.get("_id")), new BasicDBObject("$pull", new BasicDBObject("scores", secondHWObject)));
+                            }
+                            firstHWObject = null;
+                        }
+                    }
+                }                // studentsCollection.
             }
         } finally {
             System.out.print(cursor.count());
             cursor.close();
         }
     }
-
-
-    private static void massageCurrentStudent(BasicDBObject currentStudent, DBCollection collection) {
-        final BasicDBList scores = (BasicDBList) currentStudent.get("scores");
-        Iterator<Object> iterator = scores.iterator();
-        BasicDBObject oldScoreObject = null;
-        while (iterator.hasNext()) {
-            BasicDBObject object = (BasicDBObject) iterator.next();
-            if (null != object && object.get("type").toString().equalsIgnoreCase("homework")) {
-                BasicDBObject currentScore = object;
-                if (null == oldScoreObject) {
-                    oldScoreObject = currentScore;
-                } else {
-                    System.out.println(":::: homework oldscore ::::" + oldScoreObject.getDouble("score"));
-                    System.out.println(":::: homework currentscore::::" + currentScore.getDouble("score"));
-                    if (oldScoreObject.getDouble("score") < currentScore.getDouble("score")) {
-                        System.out.println(":::: to remove >>> " + oldScoreObject.getDouble("score"));
-                        collection.update(new BasicDBObject("_id", currentStudent.get("_id")), new BasicDBObject("$pull", new BasicDBObject("scores", oldScoreObject.getDouble("score"))));
-                    } else {
-                        System.out.println(":::: to remove >>> " + currentScore.getDouble("score"));
-                        collection.update(new BasicDBObject("_id", currentStudent.get("_id")), new BasicDBObject("$pull", new BasicDBObject("scores", currentScore.getDouble("score"))));
-                    }
-                    oldScoreObject = null;
-                }
-            }
-        }
-    }
-
-
-
-//    private static BasicDBObject massageCurrentStudent(BasicDBObject currentStudent) {
-//        BasicDBList scores = (BasicDBList) currentStudent.get("scores");
-//
-//        Iterator iterator = ((ArrayList) scores).iterator();
-//        while (iterator.hasNext()) {
-//            BasicDBObject object = (BasicDBObject) iterator.next();
-//            BasicDBObject nextObject = (BasicDBObject) iterator.next();
-//
-//            if (null != object && object.get("type").toString().equalsIgnoreCase("homework")) {
-//
-//                System.out.println(":::: homework this ::::" + object.getDouble("score"));
-//                System.out.println(":::: homework future ::::" + ((BasicDBObject) iterator.next()).getDouble("score"));
-//                if (object.getDouble("score") < ((BasicDBObject) iterator.next()).getDouble("score")) {
-//                    System.out.println(":::: to remove >>> " + object.getDouble("score"));
-//                    iterator.remove();
-//                } else {
-//                    System.out.println(":::: to remove >>> " + ((BasicDBObject) iterator.next()).getDouble("score"));
-//                    Object next = iterator.next();
-//                    ((BasicDBObject) iterator.next()).remove(next);
-//
-//                }
-//            }
-//        }
-//        return currentStudent;
-//    }
-
 }
